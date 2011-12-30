2dbd29e3236d62f93ad205f47eae0354710db26f
Milind S. Pandit <mspandit@imac-2.ftrdhcpuser.net>
2011-12-29 16:43:31 -0800

    Removed a file and fixed a query.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -12,7 +12,7 @@ class Tweet < ActiveRecord::Base
           :secret => SearchConfiguration.first.twitter_secret
         ).mentions(count: TWITTER_MENTIONS_PER_PAGE, page: page))
         mentions.each do |mention| 
-          unless (Tweet.find_by_tweet_id(mention["id"]) || mention["text"].index(SearchConfiguration.first.twitter_search_string).nil?)
+          unless (Tweet.find_by_tweet_id(mention["id"].to_s) || mention["text"].index(SearchConfiguration.first.twitter_search_string).nil?)
             user = User.find_or_initialize_by_twitter_id(mention["user"]["id"])
             user.twitter_screen_name = mention["user"]["screen_name"]
             user.name = mention["user"]["name"]
